d269bacd7f4713a2dc405700bfd843ec49b2fd18
Pia Sotlar <pia.sotli@gmail.com>
2025-12-21 12:18:56 +0100

    Helm charts

--- app/elastic/client.py
+++ app/elastic/client.py
@@ -1,6 +1,12 @@
 import os
 from elasticsearch import AsyncElasticsearch
 
-#for connecting to elasticsearch
-ES_HOST = os.getenv("ELASTICSEARCH_HOST", "http://elasticsearch:9200")
-client = AsyncElasticsearch(hosts=[ES_HOST])
+ES_HOST = os.getenv("ELASTICSEARCH_HOST", "https://quickstart-es-http:9200")
+ES_USER = os.getenv("ELASTICSEARCH_USER", "elastic")
+ES_PASS = os.getenv("ELASTICSEARCH_PASSWORD")
+
+client = AsyncElasticsearch(
+    hosts=[ES_HOST],
+    basic_auth=(ES_USER, ES_PASS),
+    verify_certs=False,  # self-signed cert, za dev v redu
+)
